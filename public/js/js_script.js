e241efd7de39febd87e7c50f4805c19e4b387fd9
Sanne Lindqvist <sanne.lindqvist.7109@student.uu.se>
2020-02-03 19:02:43 +0100

    Working on using javascript to generate the burgers

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -13,31 +13,53 @@ function menuItem(name, kcal, gluten, lactose, imgpath) {
 let burger1 = new menuItem("Double Cheese Burger", 440, true, true, "./img/burger1.jpg")
 let burger2 = new menuItem("Crispy Halloumi Burger", 430, true, true, "./img/burger2.jpg")
 let burger3 = new menuItem("Mushroom Bean Burger", 400, false, false, "./img/burger3.jpg")
-let burger4 = new menuItem("A new burger", 440, false, true, "")
-let burger5 = new menuItem("Another new burger", 440, true, false, "")
 
-var menu = [burger1, burger2, burger3, burger4, burger5]
+var menu = [burger1, burger2, burger3]
 
 let i = 0;
 for (i; i < menu.length; i++) {
-    var burgerParagraph = document.createElement("p");
-    burgerParagraph.innerHTML = menu[i].info();
+
+    var h = document.createElement("H3");
+    //var n = document.createTextElement(menu[i].name);
+    //h.appendChild(n);
+    h.innerHTML = menu[i].name;
+    document.getElementById("test").appendChild(h);
+
+    var image = document.createElement("img");
+    image.setAttribute("src", menu[i].imgpath);
+    image.setAttribute("alt", menu[i].name);
+    image.setAttribute("title", menu[i].name);
+    image.setAttribute("width", 200);
+    document.getElementById("test").appendChild(image);
+
+    var list = document.createElement("ul");
+
+    var kcal = document.createElement("li");
+    var text = document.createTextNode(menu[i].kcal + " kCal");
+    kcal.appendChild(text);
+    list.appendChild(kcal);
+
+    var allergy = document.createElement("section");
+    allergy.setAttribute("class", "allergy");
+
 
     if (menu[i].gluten) {
-        burgerParagraph.innerHTML = burgerParagraph.innerHTML + " Contains gluten ";
+        //burgerParagraph.innerHTML = burgerParagraph.innerHTML + " Contains gluten ";
+        var gluten = document.createElement("li");
+        text = document.createTextNode("Contains gluten");
+        gluten.appendChild(text);
+        allergy.appendChild(gluten);
     }
 
     if (menu[i].lactose) {
-        burgerParagraph.innerHTML = burgerParagraph.innerHTML + " Contains lactose ";
+        //burgerParagraph.innerHTML = burgerParagraph.innerHTML + " Contains lactose ";
+        var lactose = document.createElement("li");
+        text = document.createTextNode("Contains lactose");
+        lactose.appendChild(text);
+        allergy.appendChild(lactose);
     }
 
-    document.getElementById("myID").appendChild(burgerParagraph);
+    list.appendChild(allergy);
 
-
+    document.getElementById("test").appendChild(list);
 }
-
-console.log(burger1.info());
-console.log(burger2.info());
-console.log(burger3.info());
-console.log(burger4.info());
-console.log(burger5.info());
