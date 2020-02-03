a1d1d4f0dbf18c751928c2bca4e33c9bbb59a05d
Sanne Lindqvist <sanne.lindqvist.7109@student.uu.se>
2020-02-03 16:04:34 +0100

    loops in js

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -10,31 +10,31 @@ function menuItem(name, kcal, gluten, lactose, imgpath) {
     }
 }
 
-let burger1 = new menuItem("Double Cheese Burger", 440, "gluten", "lactose", "./img/burger1.jpg")
-let burger2 = new menuItem("Crispy Halloumi Burger", 430, "gluten", "lactose", "./img/burger2.jpg")
-let burger3 = new menuItem("Mushroom Bean Burger", 400, "gluten free", "lactose free", "./img/burger3.jpg")
-let burger4 = new menuItem("A new burger", 440, "gluten", "lactose", "")
-let burger5 = new menuItem("Another new burger", 440, "gluten", "lactose", "")
+let burger1 = new menuItem("Double Cheese Burger", 440, true, true, "./img/burger1.jpg")
+let burger2 = new menuItem("Crispy Halloumi Burger", 430, true, true, "./img/burger2.jpg")
+let burger3 = new menuItem("Mushroom Bean Burger", 400, false, false, "./img/burger3.jpg")
+let burger4 = new menuItem("A new burger", 440, false, true, "")
+let burger5 = new menuItem("Another new burger", 440, true, false, "")
 
-var burgerParagraph = document.createElement("p");
-burgerParagraph.innerHTML = burger1.info();
-document.getElementById("myID").appendChild(burgerParagraph);
+var menu = [burger1, burger2, burger3, burger4, burger5]
 
-burgerParagraph = document.createElement("p");
-burgerParagraph.innerHTML = burger2.info();
-document.getElementById("myID").appendChild(burgerParagraph);
+let i = 0;
+for (i; i < menu.length; i++) {
+	var burgerParagraph = document.createElement("p");
+	burgerParagraph.innerHTML = menu[i].info();
 
-burgerParagraph = document.createElement("p");
-burgerParagraph.innerHTML = burger3.info();
-document.getElementById("myID").appendChild(burgerParagraph);
+  if (menu[i].gluten) {
+    burgerParagraph.innerHTML = burgerParagraph.innerHTML + " Contains gluten ";
+  }
 
-burgerParagraph = document.createElement("p");
-burgerParagraph.innerHTML = burger4.info();
-document.getElementById("myID").appendChild(burgerParagraph);
+  if (menu[i].lactose) {
+    burgerParagraph.innerHTML = burgerParagraph.innerHTML + " Contains lactose ";
+  }
 
-burgerParagraph = document.createElement("p");
-burgerParagraph.innerHTML = burger5.info();
-document.getElementById("myID").appendChild(burgerParagraph);
+	document.getElementById("myID").appendChild(burgerParagraph);
+
+
+}
 
 console.log(burger1.info());
 console.log(burger2.info());
