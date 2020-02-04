0665dfdce52b63ab9e7f06fcbd03cfde572760bd
Sanne Lindqvist <sanne.lindqvist.7109@student.uu.se>
2020-02-04 20:11:16 +0100

    checkbox in progress

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -8,6 +8,7 @@ const vm = new Vue({
     house: "",
     payment: "",
     gender: "",
+    burger: "",
     buttonClicked: false,
     },
 
@@ -36,41 +37,9 @@ const vm = new Vue({
             console.log("Read " + payment);
             console.log("Read " + gender);
 
-            /*var text = document.createTextNode(fullName);
-            document.getElementById("sentOrder").appendChild(text);*/
             this.buttonClicked = true;
 
             return [fullName, email, street, house, payment, gender];
         }
     }
 })
-/*
-const vm2 = new Vue({
-      el: '#orders',
-        methods: {
-            markDone: function() {
-                var fullName = document.getElementById("fullname").value;
-                var email = document.getElementById("email").value;
-                var street = document.getElementById("street").value;
-                var house = document.getElementById("house").value;
-
-                var radios = document.getElementsByName("gender");
-                var gender;
-                for (i = 0; i < radios.length; i++) {
-                    if (radios[i].checked) {
-                        gender = radios[i].value;
-                        break;
-                    }
-                }
-
-                console.log("Button clicked!");
-                console.log("Read " + fullName);
-                console.log("Read " + email);
-                console.log("Read " + street);
-                console.log("Read " + house);
-                console.log("Read " + gender);
-
-                return [fullName, email, street, house, gender];
-            }
-        }
-})*/
