35b615d41abac87730bbed29df0d371e07a545b0
Sanne Lindqvist <sanne.lindqvist.7109@student.uu.se>
2020-02-04 18:09:06 +0100

    Now using json for vue

---
+++ public/js/menu.js
@@ -0,0 +1,42 @@
+let food = [
+    {
+        "name": "Double Cheese Burger",
+        "kcal": 440,
+        "lactose": true,
+        "gluten": true,
+        "img": "./img/burger1.jpg",
+        "box": "box a",
+    },
+    {
+        "name": "Crispy Halloumi Burger",
+        "kcal": 430,
+        "lactose": true,
+        "gluten": true,
+        "img": "./img/burger2.jpg",
+        "box": "box b",
+    },
+    {
+        "name": "Mushroom Bean Burger",
+        "kcal": 400,
+        "lactose": false,
+        "gluten": false,
+        "img": "./img/burger3.jpg",
+        "box": "box c",
+    },
+    {
+        "name": "A New Burger",
+        "kcal": 400,
+        "lactose": true,
+        "gluten": false,
+        "img": "./img/burger3.jpg",
+        "box": "box c",
+    },
+    {
+        "name": "Another New Burger",
+        "kcal": 400,
+        "lactose": false,
+        "gluten": true,
+        "img": "./img/burger3.jpg",
+        "box": "box c",
+    }
+]
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,27 +1,6 @@
-function menuItem(name, kcal, gluten, lactose, imgpath, box) {
-    this.name = name;
-    this.kcal = kcal;
-    this.gluten = gluten;
-    this.lactose = lactose;
-    this.imgpath = imgpath;
-    this.box = box;
-
-    this.info = function() {
-        return this.name + " " + this.kcal + " kCal ";
-    }
-}
-
-let burger1 = new menuItem("Double Cheese Burger", 440, true, true, "./img/burger1.jpg", "box a")
-let burger2 = new menuItem("Crispy Halloumi Burger", 430, true, true, "./img/burger2.jpg", "box b")
-let burger3 = new menuItem("Mushroom Bean Burger", 400, false, false, "./img/burger3.jpg", "box c")
-let burger4 = new menuItem("A new burger", 440, false, true, "./img/burger3.jpg", "box c")
-let burger5 = new menuItem("Another new burger", 440, true, false, "./img/burger3.jpg", "box c")
-
-var menu = [burger1, burger2, burger3, burger4, burger5]
-
 const vm = new Vue({
   el: '#vuewrapper',
   data: {
-  	menu: menu,
+    food: food,
   }
 })
